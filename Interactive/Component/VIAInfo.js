eb735b1e7608c2ace96de29c7e4682bcd611885e
Mark Black <mark.prestwick@gmail.com>
2016-07-25 13:36:32 -0500

    Added slider for control

--- Interactive/Component/VIAInfo.js
+++ Interactive/Component/VIAInfo.js
@@ -3,11 +3,14 @@ NationalInstruments.HtmlVI.viName = '_%44esktopWeb%2Egvi';
 
 //inputs: 
 NationalInstruments.HtmlVI.inputDataItem = {};
+NationalInstruments.HtmlVI.inputDataItem['di_992']='Number';
 
 //outputs:
 NationalInstruments.HtmlVI.outputDataItem = {};
+NationalInstruments.HtmlVI.outputDataItem['di_152']='Array';
 NationalInstruments.HtmlVI.outputDataItem['di_15']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_1027']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_30']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_141']='Number';
 NationalInstruments.HtmlVI.outputDataItem['di_126']='Number';
-NationalInstruments.HtmlVI.outputDataItem['di_152']='Array';
+NationalInstruments.HtmlVI.outputDataItem['di_1003']='Boolean';
