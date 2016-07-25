b02e6b1b21121be1d607e92052131a18cd4b73c1
Mark Black <mark.prestwick@gmail.com>
2016-07-25 11:18:03 -0500

    First build output

---
+++ Interactive/Component/VIAInfo.js
@@ -0,0 +1,13 @@
+//  via info  
+NationalInstruments.HtmlVI.viName = '_%44esktopWeb%2Egvi';
+
+//inputs: 
+NationalInstruments.HtmlVI.inputDataItem = {};
+
+//outputs:
+NationalInstruments.HtmlVI.outputDataItem = {};
+NationalInstruments.HtmlVI.outputDataItem['di_15']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_30']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_141']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_126']='Number';
+NationalInstruments.HtmlVI.outputDataItem['di_152']='Array';
